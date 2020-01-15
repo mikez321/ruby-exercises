d7fa24f1fc58dc2c08ecbd508a869669e7841770
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-15 09:15:44 -0700

    Fix all issues and create code for pirate test

--- mythical-creatures/test/pirate_test.rb
+++ mythical-creatures/test/pirate_test.rb
@@ -10,31 +10,26 @@ class PirateTest < Minitest::Test
   end
 
   def test_can_have_different_name
-    skip
     pirate = Pirate.new("Blackbeard")
     assert_equal "Blackbeard", pirate.name
   end
 
   def test_is_a_scallywag_by_default
-    skip
     pirate = Pirate.new("Jack")
     assert_equal 'Scallywag', pirate.job
   end
 
   def test_in_not_always_a_scallywag
-    skip
     pirate = Pirate.new("Jack", "Cook")
     assert_equal "Cook", pirate.job
   end
 
   def test_isnt_cursed_by_default
-    skip
     pirate = Pirate.new("Jack")
     refute pirate.cursed?
   end
 
   def test_becomes_cursed_after_enough_heinous_acts
-    skip
     pirate = Pirate.new("Jack")
     refute pirate.cursed?
     pirate.commit_heinous_act
@@ -46,13 +41,18 @@ class PirateTest < Minitest::Test
   end
 
   def test_a_pirate_has_booty
-    skip
+    pirate = Pirate.new("Roger")
+    assert_equal 0, pirate.booty
     # create a pirate
     # check that the pirate starts with 0 booty
   end
 
   def test_a_pirate_gets_100_booty_for_robbing_ships
-    skip
+    pirate = Pirate.new("Morgan")
+    3.times do
+      pirate.rob_ship
+    end
+    assert_equal 300, pirate.booty
     # create a pirate
     # rob some ships
     # check that the pirate got 100 booty for each ship it robbed
