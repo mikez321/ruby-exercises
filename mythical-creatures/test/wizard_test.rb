2ec44689fb7284422e0420e3f9670be40b4de0e8
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-16 09:54:49 -0700

    Add code and fix issues

--- mythical-creatures/test/wizard_test.rb
+++ mythical-creatures/test/wizard_test.rb
@@ -10,49 +10,51 @@ class WizardTest < Minitest::Test
   end
 
   def test_can_have_different_name
-    skip
     wizard = Wizard.new("Alex")
     assert_equal "Alex", wizard.name
   end
 
   def test_is_bearded_by_default
-    skip
     wizard = Wizard.new("Ben")
     assert wizard.bearded?
   end
 
   def test_is_not_always_bearded
-    skip
     wizard = Wizard.new("Valerie", bearded: false)
     refute wizard.bearded?
   end
 
   def test_has_root_powers
-    skip
     wizard = Wizard.new("Sarah", bearded: false)
     assert_equal "sudo chown ~/bin", wizard.incantation("chown ~/bin")
   end
 
   def test_has_lots_of_root_powers
-    skip
     wizard = Wizard.new("Rob", bearded: false)
     assert_equal "sudo rm -rf /home/mirandax", wizard.incantation("rm -rf /home/mirandax")
   end
 
   def test_starts_rested
-    skip
+    wizard = Wizard.new("Earl", bearded: true)
+    assert wizard.rested?
     # create wizard
     # .rested? returns true
   end
 
   def test_can_cast_spells
-    skip
+    wizard = Wizard.new("Dan")
+    assert_equal "MAGIC MISSLE!", wizard.cast("magic missle")
     # create wizard
     # .cast returns "MAGIC MISSILE!"
   end
 
   def test_gets_tired_after_casting_three_spells
-    skip
+    wizard = Wizard.new("Calvin")
+    assert_equal "LEVITATE TRUCK!", wizard.cast("levitate truck")
+    assert_equal "SUPER STRENGTH!", wizard.cast("super strength")
+    assert wizard.rested?
+    assert_equal "THROW SHADE!", wizard.cast("throw shade")
+    refute wizard.rested?
     # create wizard
     # casts spell twice
     # check wizard is rested
@@ -61,4 +63,3 @@ class WizardTest < Minitest::Test
   end
 
 end
-
