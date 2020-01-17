0b11a19fb4dc36c33ff64264dc467c19606e32dd
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-17 13:44:55 -0700

    Remove skips and add final two tests

--- mythical-creatures/test/medusa_test.rb
+++ mythical-creatures/test/medusa_test.rb
@@ -10,23 +10,20 @@ class MedusaTest < Minitest::Test
   end
 
   def test_when_first_created_she_has_no_statues
-    skip
     medusa = Medusa.new("Cassiopeia")
     assert medusa.statues.empty?
   end
 
   def test_when_staring_at_a_person_she_gains_a_statue
-    skip
     medusa = Medusa.new("Cassiopeia")
     victim = Person.new("Perseus")
-
     medusa.stare(victim)
+
     assert_equal 1, medusa.statues.count
     assert_equal "Perseus", medusa.statues.first.name
   end
 
   def test_when_staring_at_a_person_that_person_turns_to_stone
-    skip
     medusa = Medusa.new("Cassiopeia")
     victim = Person.new("Perseus")
 
@@ -36,13 +33,38 @@ class MedusaTest < Minitest::Test
   end
 
   def test_can_only_have_three_victims
-    skip
-    # your code here
+    medusa = Medusa.new("Cassiopeia")
+    victim = Person.new("Perseus")
+    victim2 = Person.new("Aesop")
+    victim3 = Person.new("Diogenes")
+    victim4 = Person.new("Leander")
+
+    medusa.stare(victim)
+    medusa.stare(victim2)
+    medusa.stare(victim3)
+    assert_equal 3, medusa.statues.count
+    medusa.stare(victim4)
+    assert_equal 3, medusa.statues.count
   end
 
   def test_if_a_fourth_victim_is_stoned_first_is_unstoned
-    skip
-    # your code here
+    medusa = Medusa.new("Cassiopeia")
+    victim = Person.new("Perseus")
+    victim2 = Person.new("Aesop")
+    victim3 = Person.new("Diogenes")
+    victim4 = Person.new("Leander")
+
+    medusa.stare(victim)
+    medusa.stare(victim2)
+    medusa.stare(victim3)
+
+    assert_equal "Perseus", victim.name
+    assert victim.stoned?
+
+    medusa.stare(victim4)
+
+    assert_equal "Perseus", victim.name
+    refute victim.stoned?
   end
 
 end
