0cfe81b93c57cd89755f604610b714134ebf11c0
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-21 09:11:26 -0700

    Remove unnecessary pirate code

--- mythical-creatures/lib/pirate.rb
+++ mythical-creatures/lib/pirate.rb
@@ -1,27 +1,24 @@
 class Pirate
-  attr_reader :name, :job, :cursed, :number_heinous_acts, :booty
-  attr_writer :cursed, :number_heinous_acts, :booty
-  def initialize(name, job = "Scallywag", cursed = false, booty = 0)
+  attr_reader :name, :job, :cursed, :heinous_acts, :booty
+  def initialize(name, job = "Scallywag")
     @name = name
     @job = job
-    @cursed = cursed
-    @number_heinous_acts = 0
-    @booty = booty
+    @cursed = false
+    @heinous_acts = 0
+    @booty = 0
   end
 
   def cursed?
-    self.cursed
+    cursed
   end
 
   def commit_heinous_act
-    self.number_heinous_acts += 1
-    if self.number_heinous_acts >= 3
-      @cursed = true
-    end
+    @heinous_acts += 1
+    @cursed = true if heinous_acts >= 3
   end
 
   def rob_ship
-    self.booty += 100
+    @booty += 100
   end
 
 end
