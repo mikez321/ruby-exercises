a8c8613e608adf8ce6815d0ddb1ca4c8501ac3ae
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-21 09:30:50 -0700

    Rewrite hobbit class

--- mythical-creatures/lib/hobbit.rb
+++ mythical-creatures/lib/hobbit.rb
@@ -1,44 +1,31 @@
 class Hobbit
-  attr_reader :name, :disposition, :age, :adult, :short
-  attr_writer :age, :adult
-  def initialize(name, disposition = "homebody", age = 0, adult = false, short = true)
+  attr_reader :name, :disposition, :age, :short
+  def initialize(name, disposition = "homebody")
     @name = name
     @disposition = disposition
-    @age = age
+    @age = 0
     @adult = false
     @short = true
   end
 
   def celebrate_birthday
-    self.age += 1
+    @age += 1
   end
 
   def adult?
-    if self.age < 33
-      false
-    else
-      true
-    end
+    return @adult = true if age > 32
   end
 
   def old?
-    if self.age >= 101
-      true
-    else
-      false
-    end
+    true if age > 100
   end
 
   def has_ring?
-    if self.name == "Frodo"
-      true
-    else
-      false
-    end
+    true if name == "Frodo"
   end
 
   def is_short?
-    self.short
+    short
   end
 
 end
