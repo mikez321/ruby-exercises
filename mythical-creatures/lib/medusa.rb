2474832e682b4751ecc353298359d57ead7d511e
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-21 09:35:18 -0700

    Make minor edits to medusa class

--- mythical-creatures/lib/medusa.rb
+++ mythical-creatures/lib/medusa.rb
@@ -1,34 +1,32 @@
 class Medusa
   attr_reader :name, :statues
-  attr_writer :statues
   def initialize(name, statues = [])
     @name = name
     @statues = statues
   end
 
   def stare(victim)
-    self.statues << victim
+    @statues << victim
     victim.stoned = true
     if statues.count > 3
       statues[0].stoned = false
       statues.shift
     end
-
   end
 
 end
 
 
 class Person
-  attr_reader :name, :stoned
-  attr_writer :stoned
+  attr_reader :name
+  attr_accessor :stoned
   def initialize(name, stoned: false)
     @name = name
     @stoned = stoned
   end
 
   def stoned?
-    self.stoned
+    stoned
   end
 
 end
